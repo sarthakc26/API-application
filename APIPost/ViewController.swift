ba3841975f75205831e2df9da0173a8f7d96d3b0
Mac Lab 25 <maclab25@Mac-0001.local>
2023-03-28 13:32:48 +0530

    initial commit

---
+++ APIPost/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  APIPost
+//
+//  Created by Madhuri Yerudkar on 12/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
